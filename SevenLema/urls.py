c5372eb1643e96ecfe1dc5b7ff632182ce1e3b15
llxnb <54896441+llx-08@users.noreply.github.com>
2020-07-03 00:47:38 +0800

    暂存

--- SevenLema/urls.py
+++ SevenLema/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from user import views as user_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('search/', include('search.urls'))
+    path('search/', include('search.urls')),
+    path('user/register', user_views.signUpPage)
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def signUpPage(request):
+    if request.POST:
+        # 获取用户信息
+        mailbox = request.POST.get('mailbox', None)
+        account = request.POST.get('account', None)
+        password1 = request.POST.get('password', None)
+        password2 = request.POST.get()
+
+        # 模拟判断用户是否存在与数据库
+        if mailbox == 'siri' and password == '123':
+            # 登陆成功
+
+            # 把内容存入cookie
+            resp = redirect('/app01/home/')
+
+            # 设置cookie
+            resp.set_signed_cookie('account', account, salt='llxnb')
+
+            # 把数据存入session
+            request.session['account'] = account
+
+            # 响应到客户端
+            return resp
+
+        else:
+            # 账号密码不对时
+            return render(request, 'signUpPage.html',
+                          {'msg': '账号或密码有误，请检查后登录'})
+
+    # 为GET请求时，响应到客户端
+    else:
+
+        # 获取到cookie的值
+        account = request.get_signed_cookie('account', None, salt='llxnb')
+
+        # 判断account是否存在
+        if account:
+            # 存在，则自动写入到输入框
+            return render(request, 'signUpPage.html', {'account': account})
+        else:
+            # account不存在
+            return render(request, 'signUpPage.html')
+
+
+def gotoLogin(request):
+
+    return redirect('/login/')
