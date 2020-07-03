f2a6164d305529cf403fd623c418a93893646879
ShineYun <2568922497@qq.com>
2020-07-03 10:22:23 +0800

    reformat code

--- static/js/sort-dish.js
+++ static/js/sort-dish.js
@@ -1,25 +1,23 @@
-function sort_mode(shop_id,name,order,page,limit,serving){
-    var data = {
-        "shop_id":shop_id,
-        "name" : $('#header-search').value(),
-        "order" : order,
-        "page" : page,
-        "limit" : limit,
-        "serving" : serving
-    }
+function sort_mode(shop_id, name, order, page, limit, serving) {
+    const data = {
+        "shop_id": shop_id,
+        "name": $('#header-search').value(),
+        "order": order,
+        "page": page,
+        "limit": limit,
+        "serving": serving
+    };
     $.ajax({
-        url:"/search/shop",
-        data:data,
-        type:"get",
-        success:function(data){
-            var num = data.page;    //todo
-            //这里是数据
-            var userLangs = data.data;
-            //模板   绑定    数据      添加到   指定的div或者是容器里面
-            $('#each').tmpl(userLangs).appendTo('#eachList');
+        url: "/search/shop",
+        data: data,
+        type: "get",
+        success: function (data) {
+            const num = data.page;    // TODO
+            $('#each').tmpl(data.data).appendTo('#eachList');
         }
     });
 }
+
 $(document).load(function () {
     $('#header-search-button').click(sort_mode);
     $('#sme-name').click(sort_mode);    //order_id
--- static/js/sort-shop.js
+++ static/js/sort-shop.js
@@ -1,34 +1,25 @@
-/*
-    container 结构如下
-    <div class = "shop_container" id ="xxx">
-
-    </div>
-*/
-
-function sort_mode(name,order,loc_lng,loc_lat,tags,page,limit,serving){
-    var data = {
-        "name" : $('#header-search').value(),
-        "order" : order,
-        "loc_lng" : loc_lng,
-        "loc_lat" : loc_lat,
-        "tags" : tags,
-        "page" : page,
-        "limit" : limit,
-        "serving" : serving
-    }
+function sort_mode(name, order, loc_lng, loc_lat, tags, page, limit, serving) {
+    const data = {
+        "name": $('#header-search').value(),
+        "order": order,
+        "loc_lng": loc_lng,
+        "loc_lat": loc_lat,
+        "tags": tags,
+        "page": page,
+        "limit": limit,
+        "serving": serving
+    };
     $.ajax({
-        url:"/search/shop",
-        data:data,
-        type:"get",
-        success:function(data){
-            var num = data.page;    //todo
-            //这里是数据
-            var userLangs = data.data;
-            //模板   绑定    数据      添加到   指定的div或者是容器里面
-            $('#each').tmpl(userLangs).appendTo('#eachList');
+        url: "/search/shop",
+        data: data,
+        type: "get",
+        success: function (data) {
+            const num = data.page;    // TODO
+            $('#each').tmpl(data.data).appendTo('#eachList');
         }
     });
 }
+
 $(document).load(function () {
     $('#header-search-button').click(sort_mode);
     $('#sme-name').click(sort_mode);
