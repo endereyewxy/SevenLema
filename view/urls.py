a45e47d54e90ccb28b4e680f3aab0f6558dff04e
ShineYun <2568922497@qq.com>
2020-07-04 12:04:57 +0800

    complete functons: check shop serving(T/F) contact with order_button check dish serving(T/F) contact with add_to_card_list

--- view/urls.py
+++ view/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import shop, register, dish
+from .views import shop, register, dish, root
 
 urlpatterns = [
+    path('', root),
     path('register/', register),
     path('index/', shop),
     path('shop/<int:shop_id>/', dish)
