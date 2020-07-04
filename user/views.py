8300870e042e043f1cbe78991a028fc17a67a891
llxnb <54896441+llx-08@users.noreply.github.com>
2020-07-04 15:31:34 +0800

    完成注册view模块

--- user/views.py
+++ user/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from user import models
+# from user import models
 from django.db.models import Max
+from cmdb.models.user import User
 
-database = models.user.objects.all()
+# database = models.ser.objects.all()
+database = User.objects.all()
 
 
 # Create your views here.
 def signUpPage(request):
-
     database_phone = database.values('phone')
-
-    database_maxId = database.aggregate(Max('userId'))
+    #
+    # database_maxId = database.aggregate(Max('userId'))
 
     # print(database)
 
@@ -52,9 +53,9 @@ def signUpPage(request):
                 'phone': phone
             }
 
-            models.user.objects.create(
+            database.create(
                 username=username,
-                userId=int(database_maxId) + 1,
+                # userId=int(database_maxId) + 1,
                 phone=phone,
                 password=password1
             )
@@ -68,17 +69,59 @@ def signUpPage(request):
 
     else:
 
-        return render(request, 'signUpPage.html')
+        return JsonResponse({
+            'code': 101,
+            'msg': '请求错误',
+        })
 
 
 def gotoLogin(request):
-    return redirect('/login')
+    # return redirect('/login')
+    pass
 
 
 def login(request):
-
     if request.POST:
         # 获取用户信息
-        account = request.POST.get('account', None)
+        username = request.POST.get('username', None)
+        phone = request.POST.get('phone', None)
+
         password = request.POST.get('password', None)
+
+        login_usn = User.objects.filter(username=username)
+        login_pho = User.objects.filter(phone=phone)
+
+        if login_usn.exists():
+            user = login_usn.first()
+
+            if user.password == password:
+
+                return JsonResponse({
+                    'code':0,
+                    'msg':'登录成功'
+                })
+
+        elif login_pho.exists():
+
+            pho = login_pho.first()
+
+            if pho.password == password:
+
+                return JsonResponse({
+                    'code':0,
+                    'msg':'登录成功'
+                })
+
+        else:
+            return JsonResponse({
+                'code':102,
+                'msg': '用户不存在'
+            })
+
+    else:
+
+        return JsonResponse({
+            'code': 101,
+            'msg': '请求错误',
+        })
 
