6467c88b98c806c624862161542981dab661d230
StephenCurry <455283303@qq.com>
2020-07-06 20:38:38 +0800

    .

--- view/static/js/shopInfo.js
+++ view/static/js/shopInfo.js
@@ -1,17 +1,38 @@
-const load_shop_info = () => {
-    const data = {
-        name: $('#header-search').val(),
-        order: get_order(),
-        page: paginator.currentPage(),
-        limit: paginator.limit(),
-        serving: serving
-    };
-    if (get_order() === 'dist') {
-        data['loc_lng'] = locator.lng;
-        data['loc_lat'] = locator.lat;
-    }
-    miscellaneous.web.get('/search/shop/', data, (resp) => {
+function load_shop_info() {
+    data = '';
+    miscellaneous.web.get('/shop/mine', data, (resp) => {
         paginator.maximumPage(resp.page);
+        $('#shopInfo-container').html($('#shopInfo-template').tmpl(resp.data));
+    });
+}
+
+function create_dish() {
+    const data = {
+        shop_id: shop_id,
+        name: $('#name').val(),
+        image: $('#image').val(),
+        desc: $desc('#desc').val,
+        price: $('price').val(),
+    };
+    miscellaneous.web.get('/shop/mine', data, (resp) => {
+        $('#dish-container').html($('#dish-template').tmpl(resp.data)); //
+    });
+}
+
+function check_order_finish() {
+    data = {order_id: order_id};
+    miscellaneous.web.get('/order/finish', data, (resp) => {
         $('#data-container').html($('#data-template').tmpl(resp.data));
     });
-};
+}
+$(document).ready(function () {
+    paginator.change(load_shop_info);
+    load_orders();
+});
+
+$(document).ready(() => {
+    paginator.change(load_shop_info);
+    load_orders();
+    $('#logout11').click(
+        () => miscellaneous.web.post('/user/logout/', {}, () => window.location.href = '/'));
+});
--- view/static/js/viewOrders.js
+++ view/static/js/viewOrders.js
@@ -11,6 +11,13 @@ function load_orders() {
 }
 
 $(document).ready(function () {
-    paginator.change(load_shop);
+    paginator.change(load_orders);
     load_orders();
 });
+
+// $(document).ready(() => {
+//     paginator.change(load_shop);
+//     load_orders();
+//     $('#logout11').click(
+//         () => miscellaneous.web.post('/user/logout/', {}, () => window.location.href = '/'));
+// });
