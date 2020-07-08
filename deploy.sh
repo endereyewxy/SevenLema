2745b7222eb8e70bec5b10db4070fe1085915132
endereye <endereyewxy@gmail.com>
2020-07-08 18:12:31 +0800

    Fix bugs in spider

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp SevenLema-master.zip root@47.94.149.131:/root/
--- spider/generate_sql.py
+++ spider/generate_sql.py
@@ -1,4 +1,11 @@
+import hashlib
 import json
+
+
+def get_image_name(img):
+    tok = hashlib.md5(img.encode('utf-8')).hexdigest()
+    return tok + '.' + img.split('.')[-1]
+
 
 if __name__ == '__main__':
     with open('spider.json', 'r') as f:
@@ -16,23 +23,25 @@ if __name__ == '__main__':
                     f"('美团外卖{user_id}', 'f7eb3d1b66527f563e9510d7561dd4fc', 'iednycl8j2q1ou3stzb57r4xm60vh9kg', '-', 106.52031,"
                     f"29.53899, '00000000000');\n"
                 )
+            img = get_image_name(shop['image'])
             f.write(
                 f"INSERT INTO cmdb_shop"
                 f"(name, image, desc, addr, loc_lng, loc_lat, avg_price, sales, phone, serving, user_id)"
                 f"VALUES"
-                f"('{shop['name']}', '{shop['image'][-36:]}', '美团外卖', '{shop['addr']}', {shop['loc_lng']},"
-                f"{shop['loc_lat']}, {shop['avg_price']}, 0, '{shop['phone']}', true, {user_id});\n"
+                f"('{shop['name']}', '{img}', '美团外卖', '{shop['addr']}', {shop['loc_lng']},"
+                f"{shop['loc_lat']}, {shop['avg_price']}, 0, '{shop['phone']}', 1, {user_id});\n"
             )
-            images.append(shop['image'])
+            images.append((img, shop['image']))
             shop_id += 1
             for dish in shop['dishes']:
+                img = get_image_name(shop['image'])
                 f.write(
                     f"INSERT INTO cmdb_dish"
                     f"(name, image, desc, price, sales, serving, shop_id)"
                     f"VALUES"
-                    f"('{dish['name']}', '{dish['image'][-36:]}', '美团外卖', {dish['price']}, 0, true, {shop_id});\n"
+                    f"('{dish['name']}', '{img}', '美团外卖', {dish['price']}, 0, 1, {shop_id});\n"
                 )
-                images.append(dish['image'])
+                images.append((img, dish['image']))
                 dish_id += 1
     with open('images.json', 'w') as f:
         json.dump(images, f)
